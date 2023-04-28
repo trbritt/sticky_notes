2ef3e8c801d29afb78e3a829aedde88133cb47db
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-04-28 14:51:28 -0400

    [WIP]setting unsetting bg color

--- wrapper.go
+++ wrapper.go
@@ -3,18 +3,28 @@ package main
 import (
 	"os/exec"
 	"os"
+
+    
 	"log"
+    "fmt"
 )
 func main() {
-	currentWorkingDirectory, errcwd := os.Getwd()
-    if errcwd != nil {
-        log.Fatal(errcwd)
-    }
-	cmd := exec.Command("/usr/bin/gnome-terminal", "--window-with-profile","Github","--","bash","-c",currentWorkingDirectory+"/driver/gonotes_driver")
-
-    err := cmd.Run()
+    out, err := exec.Command("/usr/bin/dconf", "read", "/org/gnome/terminal/legacy/profiles:/:54cf545b-ecbf-4289-b9dd-56f3381de31b/background-color").Output()
 
     if err != nil {
         log.Fatal(err)
     }
+    fmt.Printf("The color is %s\n", out)
+	currentWorkingDirectory, errcwd := os.Getwd()
+    if errcwd != nil {
+        log.Fatal(errcwd)
+    }
+	cmd := exec.Command("/usr/bin/gnome-terminal", "--window-with-profile","Default","--","bash","-c",currentWorkingDirectory+"/driver/gonotes_driver")
+
+    err = cmd.Run()
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
 }
