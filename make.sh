af6a6ae6a1d167b7a6434662a0bacdb925a6fff6
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-04-30 21:00:22 -0400

    utility to compile + execute

---
+++ make.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+# .PHONY: driver all main
+
+# all: driver main 
+# ROOT_DIR:=$(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))
+
+# driver:
+# 	$(CD) /home/trbritt/Desktop$(ROOT_DIR)/driver 
+# 	go build -o gonotes_driver
+# 	$(CD) ..
+
+# main:
+# 	go build -o gonotes 
+
+cd driver
+go build -o gonotes_driver
+cd ..
+go build -o gonotes
+./gonotes
