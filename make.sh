21a7f4bd29dba83918260fc8c5479f46c12e7779
Tristan Britt <tristan.britt@mail.mcgill.ca>
2023-05-01 21:26:32 -0400

    remove unnecessary comments

--- make.sh
+++ make.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/bash
-# .PHONY: driver all main
-
-# all: driver main 
-# ROOT_DIR:=$(shell dirname $(realpath $(firstword $(MAKEFILE_LIST))))
-
-# driver:
-# 	$(CD) /home/trbritt/Desktop$(ROOT_DIR)/driver 
-# 	go build -o gonotes_driver
-# 	$(CD) ..
-
-# main:
-# 	go build -o gonotes 
 
 cd /home/trbritt/Desktop/projects/sticky_notes/driver
 if [ ! -f go.mod ]; then
